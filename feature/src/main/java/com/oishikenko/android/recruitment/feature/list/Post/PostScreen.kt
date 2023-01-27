162a12ef09cfc6c7be15beb5d34235531d336186
tatsukikane <15210124kt@gmail.com>
2023-01-27 16:27:08 +0900

    レシピタイプ、レシピタグの選択を実装

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/Post/PostScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/Post/PostScreen.kt
@@ -1,6 +1,7 @@
 package com.oishikenko.android.recruitment.feature.list
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
@@ -69,6 +71,18 @@ fun PostRecipeScreen(
                 }
             }
             Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                TitleAndDropdownMenuButton(
+                    title = stringResource(id = R.string.recipe_type),
+                    option1 = stringResource(id = R.string.recipe_type_main),
+                    option2 = stringResource(id = R.string.recipe_type_side),
+                    option3 = stringResource(id = R.string.recipe_type_soup)
+                )
+                TitleAndDropdownMenuButton(
+                    title = stringResource(id = R.string.recipe_tag),
+                    option1 = stringResource(id = R.string.recipe_tag_1),
+                    option2 = stringResource(id = R.string.recipe_tag_2),
+                    option3 = stringResource(id = R.string.recipe_tag_3)
+                )
                 TitleAndTextField(stringResource(id = R.string.records_detail_title_recipe_name))
                 TitleAndTextField(stringResource(id = R.string.records_detail_title_comment))
                 TitleAndTextField(stringResource(id = R.string.records_detail_title_recipe))
@@ -120,5 +134,85 @@ fun RegisterButton(
             )
         }
         Spacer(modifier = Modifier.weight(1.0f))
+    }
+}
+
+@Composable
+fun TitleAndDropdownMenuButton(
+    title: String,
+    option1: String,
+    option2: String,
+    option3: String,
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+        DropdownMenuButton(option1, option2, option3)
+    }
+}
+
+@Composable
+fun DropdownMenuButton(
+    option1: String,
+    option2: String,
+    option3: String,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+    ) {
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                painter = rememberVectorPainter(image = Icons.Default.AddCircle),
+                contentDescription = "go back",
+                modifier = Modifier.size(32.dp),
+                tint = Color(0xFF87cefa)
+            )
+        }
+        DropdownMenu(
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colors.onSurface),
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    // Do something
+                }
+            ) {
+                Text(
+                    text = option1,
+                    fontSize = 24.sp,
+                    color = Color.White,
+                )
+            }
+            DropdownMenuItem(
+                onClick = {}
+            ) {
+                Text(
+                    text = option2,
+                    fontSize = 24.sp,
+                    color = Color.White,
+                )
+            }
+            DropdownMenuItem(
+                onClick = {}
+            ) {
+                Text(
+                    text = option3,
+                    fontSize = 24.sp,
+                    color = Color.White,
+                )
+            }
+        }
     }
 }
